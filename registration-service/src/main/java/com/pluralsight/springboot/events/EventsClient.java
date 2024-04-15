e8d45c312d4f1d50431609dc47ef9b49834efcbd
walterc98 <walter.condori.10@gmail.com>
2024-04-15 15:09:11 -0400

    7th Commit: Adding PathVariables in the EventsClient.java interface

--- registration-service/src/main/java/com/pluralsight/springboot/events/EventsClient.java
+++ registration-service/src/main/java/com/pluralsight/springboot/events/EventsClient.java
@@ -1,12 +1,13 @@
 package com.pluralsight.springboot.events;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.service.annotation.GetExchange;
 
 public interface EventsClient {
 
     @GetExchange("/products/{id}")
-    Product getProductById(int productId);
+    Product getProductById(@PathVariable("id") int productId);
 
     @GetExchange("/events/{id}")
-    Event getEventById(int eventId);
+    Event getEventById(@PathVariable("id") int eventId);
 }
